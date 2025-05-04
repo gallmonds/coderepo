de4a288e7c7b9a1e9ff67e3f3705b337fbbd878d
gallmonds <gallmonds98@gmail.com>
2025-05-04 18:48:16 -0400

    feat: Add new Angular directory organisation and integration with PostgresSQL

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/core/config/app.config';
+import { appConfig } from './app/config/app.config';
 import { AppComponent } from './app/components/app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
