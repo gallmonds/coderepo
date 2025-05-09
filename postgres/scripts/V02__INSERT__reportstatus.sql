0cc1b12e66777794534bd53dab864a25767de778
gallmonds <gallmonds98@gmail.com>
2025-05-09 11:30:53 -0400

    feat: Set config to apply sql files to postgres on first run

---
+++ postgres/scripts/V02__INSERT__reportstatus.sql
@@ -0,0 +1,3 @@
+INSERT INTO reportstatus(status_name, version) VALUES('Completed', 1);
+INSERT INTO reportstatus(status_name, version) VALUES('Pending Revision', 1);
+INSERT INTO reportstatus(status_name, version) VALUES('In revision', 1);
