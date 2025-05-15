6d8927e1faaffd666e51182cd5ad659e13a377d6
gallmonds <gallmonds98@gmail.com>
2025-05-14 20:47:11 -0400

    feat: Add roles initial data

---
+++ postgres/scripts/V07__INSERT__roles.sql
@@ -0,0 +1,4 @@
+INSERT INTO role(role_name, version) VALUES('Logged User', 1);
+INSERT INTO role(role_name, version) VALUES('Assistant', 1);
+INSERT INTO role(role_name, version) VALUES('Administrator', 1);
+INSERT INTO role(role_name, version) VALUES('Maintainers', 1);
