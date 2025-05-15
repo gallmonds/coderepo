a3096987a7d1e2fbb157a4670599f81e2ee3e884
gallmonds <gallmonds98@gmail.com>
2025-05-14 20:47:43 -0400

    feat: Add roles-permissions relational initial data

---
+++ postgres/scripts/V08__INSERT__roles_permissions.sql
@@ -0,0 +1,97 @@
+-- Logged User
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 1, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 2, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 3, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 4, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 5, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 6, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 7, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 8, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 9, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 10, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 11, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 11, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 13, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 14, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 15, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 16, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 17, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 18, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 19, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 20, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 21, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 22, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 23, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 24, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (1, 25, 1);
+
+-- Assistant
+
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (2, 26, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (2, 28, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (2, 29, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (2, 30, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (2, 37, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (2, 41, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (2, 42, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (2, 43, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (2, 44, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (2, 45, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (2, 46, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (2, 48, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (2, 49, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (2, 50, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (2, 51, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (2, 52, 1);
+
+-- Administrator
+
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (3, 26, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (3, 28, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (3, 29, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (3, 30, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (3, 37, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (3, 41, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (3, 42, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (3, 43, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (3, 44, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (3, 45, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (3, 46, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (3, 48, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (3, 49, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (3, 50, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (3, 51, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (3, 52, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (3, 31, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (3, 32, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (3, 36, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (3, 40, 1);
+
+-- Maintainers
+
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 26, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 28, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 29, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 30, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 37, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 41, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 42, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 43, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 44, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 45, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 46, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 48, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 49, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 50, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 51, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 52, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 31, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 32, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 36, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 40, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 27, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 33, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 34, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 38, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 39, 1);
+INSERT INTO role_permission(role_id, permission_id, version) VALUES (4, 47, 1);
