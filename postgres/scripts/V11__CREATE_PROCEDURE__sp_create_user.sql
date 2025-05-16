703e8ddfb944e3891fada19cf6973205cdca81d4
gallmonds <gallmonds98@gmail.com>
2025-05-16 16:37:04 -0400

    feat: Create password utils and sp_create_user SP

---
+++ postgres/scripts/V11__CREATE_PROCEDURE__sp_create_user.sql
@@ -0,0 +1,52 @@
+CREATE OR REPLACE PROCEDURE create_user(
+    username TEXT,
+    email TEXT,
+    password_hash TEXT,
+    password_salt TEXT
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    user_count INT;
+BEGIN
+    SELECT COUNT(*) INTO user_count
+    FROM users
+    WHERE username = username;
+    
+    IF user_count > 0 THEN
+        RAISE EXCEPTION 'USR01: The username already exists.' USING ERRCODE = 'USR01';
+    END IF;
+
+    SELECT COUNT(*) INTO user_count
+    FROM users
+    WHERE email = email;
+    
+    IF user_count > 0 THEN
+        RAISE EXCEPTION 'USR01: The email already exists.' USING ERRCODE = 'USR01';
+    END IF;
+
+    INSERT INTO dbuser (
+        pfp_id, 
+        username, 
+        email, 
+        password_hash, 
+        password_salt,
+		isflagged,
+		isbanned, 
+        audit_isdeleted, 
+        version
+    ) 
+    VALUES (
+        NULL, 
+        username, 
+        email, 
+        password_hash, 
+        password_salt,
+		0,
+		0, 
+        0,
+        1
+    );
+    
+END;
+$$;
