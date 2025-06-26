8d4019560a691106fd21a86fcafde6aae631a580
gallmonds <gallmonds98@gmail.com>
2025-06-26 12:13:38 -0400

    feat: Control register email to comply with RFC 5322

--- coderepo_api/Controllers/AuthController.cs
+++ coderepo_api/Controllers/AuthController.cs
@@ -2,6 +2,7 @@
 using coderepo_api.Repository;
 using coderepo_api.Utils;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
 
 namespace coderepo_api.Controllers
 {
@@ -20,6 +21,14 @@ namespace coderepo_api.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserRegisterDto dto)
         {
+            var emailRegex = new Regex(
+                @"(?:[a-z0-9!#$%&'*+\=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\=?^_`{|}~-]+)*|\""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)])",
+                RegexOptions.IgnoreCase
+            );
+
+            if (!emailRegex.IsMatch(dto.Email))
+                return BadRequest("Invalid email format.");
+
             var user = await _userRepository.Register(dto);
             if (user == null)
                 return BadRequest("Username or email already exists.");
